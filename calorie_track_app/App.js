e0d5dc4d7700bf2bab5464f7b27600c19aa18601
Lydia <zhao.ce1@northeastern.edu>
2023-02-25 17:52:57 -0800

    set the background color

--- calorie_track_app/App.js
+++ calorie_track_app/App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import {LinearGradient} from 'expo-linear-gradient';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import colors from './constant/colors';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient colors={[colors.lightPurple, 'transparent']}
+    style={styles.container}>
+        <SafeAreaView style={styles.view}>
+        </SafeAreaView>     
+        <StatusBar style="auto" />
+    </LinearGradient> 
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  display: 'flex',
+  flex:1,
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'center',
   },
+
+  view: {
+    width: '80%',
+  }
 });
+
---
+++ calorie_track_app/constant/colors.js
@@ -0,0 +1,9 @@
+const colors = {
+    "Purple": "rgb(158, 42, 155)",
+    "white": "rgb(255, 255, 255)",
+    "lightPurple": "rgb(255, 215, 245)",
+    "darkYellow": "#daa520",
+    "lightYellow": "#ffd700",
+
+}
+export default colors;
